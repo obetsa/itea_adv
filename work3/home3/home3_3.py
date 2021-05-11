c7c96a1236d622fd3c0aa444ed946177963a85b3
obetsa <obetsa@gmail.com>
2021-05-11 17:55:46 +0300

    begins hw_3

--- work3/home3/home3_3.py
+++ work3/home3/home3_3.py
@@ -7,3 +7,22 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 Решение покрыть тестами (опционально)."""
+
+from pathlib import Path
+path = Path(__file__).resolve().parent
+file_path = path / "new_file.txt"
+file_path1 = path / "new_file1.txt"
+
+
+
+with open(file_path, "w") as f:
+    f.write('One - 1\nTwo - 2\nThree - 3\nFour - 4')
+
+
+with open(file_path, "r") as f:
+    f.seek(0)
+    text = f.read()
+    text = text.replace("One", "Один")
+    print(f.readlines())
+    with open(file_path1, "w") as f:
+        f.write(text)
