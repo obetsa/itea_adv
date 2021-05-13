c34d969f7361c25b84a580f519834db6c3bec1b4
obetsa <obetsa@gmail.com>
2021-05-13 17:41:42 +0300

    home 4 beggin

--- work4/home4/home4_1.py
+++ work4/home4/home4_1.py
@@ -2,3 +2,30 @@
  объект-генератор, который по запросу будет возвращать значение факториала всех чисел от 0 до n.
  5! = 1 * 2 * 3 * 4 * 5 
 """
+
+try:
+    n = int(input("Enter number: "))
+except ValueError:
+    print("Not a number")
+except Exception:
+    print("what is this??)")
+
+
+def number():
+    pass
+
+def fibonacchi(n):
+    a = 1
+    b = 1
+    i = 0
+    while i < n - 2:
+        summa = a + b
+        a = b
+        b = summa
+        i = i + 1
+ 
+    print("Значення: ", b)
+
+
+fibonacchi(n)
+
--- work4/home4/home4_2.py
+++ work4/home4/home4_2.py
@@ -5,3 +5,30 @@
  Также создайте класс «Склад», экземпляр которого будет способен принимать в себя объекты техники на хранение.
  Организуйте для него протокол итерации (чтобы объекты вашего склада можно было бы перебирать).
 """
+from abc import ABC, abstractmethod
+
+
+class BaseOrgtech(ABC):
+
+    @abstractmethod
+    def ping(self):
+        pass
+
+class Printer(BaseOrgtech):
+    def __init__(self):
+        self.t_printer = t_printer
+        self.printing = printing
+
+class Scanner(BaseOrgtech):
+    def __init__(self):
+        self.t_scanner = t_scanner
+        self.scan = scan
+
+class Xerox(BaseOrgtech):
+    def __init__(self):
+        self.t_xerox = t_xerox
+        self.xero = xero
+
+
+class Sclad()
+ 
