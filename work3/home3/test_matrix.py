99f0c4698fca5a020b948a74061add07fb808e6b
obetsa <obetsa@gmail.com>
2021-06-01 16:13:56 +0300

    refact test matrix

--- work3/home3/test_matrix.py
+++ work3/home3/test_matrix.py
@@ -1,6 +1,6 @@
 import pytest
 
-from .home3_2 import Matrix
+from work3.home3.home3_2 import Matrix
 
 
 @pytest.mark.parametrize(
@@ -39,4 +39,24 @@ from .home3_2 import Matrix
 
 def matrix_add(first_matrix, second_matrix, result_matrix):
     res_matrix = Matrix(first_matrix) + Matrix(second_matrix)
+    assert res_matrix == Matrix(result_matrix)
+
+
+@pytest.mark.parametrize(
+    "first_matrix, second_matrix, result_matrix", 
+    [(
+            [[2, 2, 2],
+             [2, 2, 2],
+             [2, 2, 2]],
+            [[1, 1, 1],
+             [1, 1, 1],
+             [1, 1, 1]],
+            [[1, 1, 1],
+             [1, 1, 1],
+             [1, 1, 1]],
+        ),
+    ])
+
+def matrix_sub(first_matrix, second_matrix, result_matrix):
+    res_matrix = Matrix(first_matrix) - Matrix(second_matrix)
     assert res_matrix == Matrix(result_matrix)
