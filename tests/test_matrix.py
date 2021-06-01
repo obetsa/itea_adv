cd503dd83b775fa240635675b09fa3993c4a04a2
obetsa <obetsa@gmail.com>
2021-06-01 17:39:53 +0300

    tests

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -16,18 +16,8 @@ from work3.home3.home3_2 import Matrix
              [3, 3, 3],
              [3, 3, 3]],
         ),
+
         (
-            [[2, 2, 2],
-             [-1, -1, -1],
-             [12, 12, 12]],
-            [[-1, 1, 1],
-             [1, 0, 1],
-             [1, 1, 7]],
-            [[1, 3, 3],
-             [0, -1, 0],
-             [13, 13, 19]],
-        ),
-                (
             [[2, 2],
              [2, 2]],
             [[1, 1],
@@ -39,4 +29,24 @@ from work3.home3.home3_2 import Matrix
 
 def matrix_add(first_matrix, second_matrix, result_matrix):
     res_matrix = Matrix(first_matrix) + Matrix(second_matrix)
+    assert res_matrix == Matrix(result_matrix)
+
+
+@pytest.mark.parametrize(
+    "first_matrix, second_matrix, result_matrix", 
+    [(
+            [[2, 2, 2],
+             [2, 2, 2],
+             [2, 2, 2]],
+            [[1, 1, 1],
+             [1, 1, 1],
+             [1, 1, 1]],
+            [[1, 1, 1],
+             [1, 1, 1],
+             [1, 1, 1]],
+        ),
+    ])
+
+def matrix_sub(first_matrix, second_matrix, result_matrix):
+    res_matrix = Matrix(first_matrix) - Matrix(second_matrix)
     assert res_matrix == Matrix(result_matrix)
